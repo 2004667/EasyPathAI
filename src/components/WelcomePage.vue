14d20b670f3f887492d186f84468ba6fd2b8f781
YerassylSuleimenov <nyt20192004@gmail.com>
2024-06-21 13:53:15 +0500

    true-code_v2

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -2,40 +2,59 @@
 
 </script>
 <template>
-      <div class="flex justify-center flex-wrap " >
-        <h1 class="
-            font-['Noto_Sans'] font-medium mt-[150px] flex
-            max-[767px]:text-[34px] 
-            md:text-[80px]">
-            Добро пожаловать в
+      <div class="
+          flex justify-center flex-wrap " >
+        <div class="
+            font-['Noto_Sans'] font-medium flex flex-wrap justify-center
+            max-[767px]:text-[34px] max-[767px]:mt-[220px]
+            md:text-[80px] md:mt-[150px]">
+          <div class="
+              flex justify-center
+              max-[767px]:w-full">
+            <h1 class="
+              
+              ">
+              Добро пожаловать
+          </h1>
+          </div>
+          <h1 class="
+              flex
+              md:ml-2 md:mr-2">
+              в
+          </h1>
           <img src="../assets/images/mega_logo.svg" alt="" class="
-            md:w-[3.8125rem] md:h-[5.125rem] md:ml-2 md:mr-2 md:mt-7
-            max-[767px]:w-[1.875rem] max-[767px]:h-[2.375rem]">
-            EasyPathAI!
-        </h1>
+              md:w-[3.8125rem] md:h-[5.125rem] md:ml-2 md:mr-2 md:mt-7
+              max-[767px]:w-[1.875rem] max-[767px]:h-[2.375rem] max-[767px]:mt-2.5 max-[767px]:ml-1 max-[767px]:mr-1">
+          <h1 class="flex">
+              EasyPathAI!
+          </h1>
+        </div>
         <div class="
             flex w-full justify-center">
           <p class="
-            flex drop-shadow-lg font-['Noto_Sans'] font-medium mt-[89px]
-            max-[767px]:text-[10px] 
-            md:text-[36px]">
+            flex drop-shadow-lg font-['Noto_Sans'] font-medium
+            max-[767px]:text-[10px] max-[767px]:mt-[25px]
+            md:text-[36px] md:mt-[89px]">
             используй силу ИИ для подготовки к собеседованиям
           </p>
         </div>
         <button id="signingoogle" class="
-            rounded-md flex items-center justify-center mt-[89px] border-[#E96852]
-            md:border-2 md:w-[25.6875rem] md:h-[4.125rem]
-            max-[767px]:border-2 max-[767px]:w-[9.25rem] max-[767px]:h-[1.425rem] max-[767px]:mt-[50px]">
-          <img class="mr-3" src="../assets/images/google_logo.svg" alt="">
+            rounded-md flex items-center justify-center border-[#E96852]
+            md:border-2 md:w-[25.6875rem] md:h-[4.125rem] md:mt-[89px] 
+            max-[767px]:border-2 max-[767px]:w-[9.25rem] max-[767px]:h-[1.425rem] max-[767px]:mt-[25px]">
+          <img class="
+            md:mr-3
+            max-[767px]:w-[15px] max-[767px]:h-[17px] max-[767px]:mr-2
+            " src="../assets/images/google_logo.svg" alt="">
           <div class="
             border-r-2 border-[#E96852] 
             md:h-[62px]
-            max-[767px]:h-5 max-[767px]:w-7 ">
+            max-[767px]:h-[20px]">
           </div>
           <p class="
-            max-[767px]:text-[11px] max-[767px]:ml-1  
-            md:text-[33px] md:ml-6 
-            mr-4">
+            max-[767px]:text-[11px] max-[767px]:ml-2  
+            md:text-[33px] md:ml-6 md:mr-4
+            max-[767px]:mr-1">
             Sign in with Google
           </p>
         </button>
