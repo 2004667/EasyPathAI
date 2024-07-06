6284fc95c9b1414062adf5b735a207d309ee3f9f
YerassylSuleimenov <nyt20192004@gmail.com>
2024-07-06 11:09:26 +0500

    true_code_v20

--- src/components/InfoAIChat.vue
+++ src/components/InfoAIChat.vue
@@ -6,28 +6,28 @@
             flex justify-center flex-wrap 
             w-full bg-[#2F2B2B] 
             md:h-[450px] md:mt-[220px] md:mb-[200px]
-            max-[767px]:mt-[190px] max-[767px]:mb-[200px] max-[767px]:h-[300px] max-[767px]:items-center">
+            max-[767px]:mt-[190px] max-[767px]:mb-[200px] max-[767px]:h-[400px] max-[767px]:items-center">
         <div class="flex justify-center md:items-center flex-wrap">
             <div class="
                     flex flex-wrap">
                 <div class="w-full max-[767px]:flex max-[767px]:justify-center max-[767px]:items-center">
                     <img class="
                         md:w-[96px] md:h-[72px] md:mb-[15px]
-                        max-[767px]:w-[35px] max-[767px]:h-[36px] max-[767px]:mb-[10px] animate-pulse"  
+                        max-[767px]:w-[42px] max-[767px]:h-[43px] max-[767px]:mb-[10px] animate-pulse"  
                         src="/images/yellow_light_main.svg" alt="">
                 </div>
                 <h1 class="
                     font-['Noto_Sans'] font-bold text-[#FFFFFF]
                     md:text-[33px] md:mr-[50px]
-                    max-[767px]:w-full max-[767px]:text-[21px] max-[767px]:text-center max-[767px]:mb-[15px]">
+                    max-[767px]:w-full max-[767px]:text-[24px] max-[767px]:text-center max-[767px]:mb-[15px]">
                     Ваш Личный AI Ассистент <br> для Собеседований
                 </h1>
             </div>
             <h1 class="
                 font-['Noto_Sans'] font-medium text-[#FFFFFF]
                 md:text-[18px] md:mt-[110px]
-                max-[767px]:text-[10px]">
-                Получите мгновенные ответы на вопросы в режиме реального <br> времени, персонализированные советы и рекомендации по <br> подготовке, помощь в составлении ответов на сложные вопросы и <br> постоянную поддержку в любое время.
+                max-[767px]:text-[13px] max-[767px]:w-8/12 ">
+                Получите мгновенные ответы на вопросы в режиме реального <br class="max-[767px]:hidden"> времени, персонализированные советы и рекомендации по <br class="max-[767px]:hidden"> подготовке, помощь в составлении ответов на сложные вопросы и <br class="max-[767px]:hidden"> постоянную поддержку в любое время.
             </h1>
         </div>
         <div class="flex w-full max-[767px]:justify-center max-[767px]:">
