93e97710b8ecee43a582f9b775b70f9cff3ac140
YerassylSuleimenov <nyt20192004@gmail.com>
2024-07-09 16:56:39 +0500

    true_code_23

--- src/components/ProfessionTags.vue
+++ src/components/ProfessionTags.vue
@@ -1,15 +1,14 @@
 <template>
-    <div class=" flex justify-center items-center flex-wrap md:w-8/12 space-x-4 border rounded md:min-h-[90px] bg-[#FCFCFC]">
+    <div class=" flex justify-center items-center flex-wrap md:w-8/12 border space-x-3 rounded max-[767px]:min-h-[180px] md:min-h-[100px] bg-[#FCFCFC]">
         <div class="w-full flex items-center justify-center space-x-1">
             <h1 class="text-[#B1ABAB] text-[19px] font-bold text-center">Фильтры</h1>
             <img class="w-[15px] mt-[6px]" src="/images/filter.svg" alt="">
         </div>
-        <OneTag class="bg-[#F0EFEF] rounded-lg min-w-[100px] h-[30px]" v-for="(proftag,index) in proftags" :key="index" :name="proftag.name" />
+        <OneTag class="bg-[#F0EFEF] rounded-lg min-w-[100px] h-[30px] mb-[6px]" v-for="(proftag,index) in proftags" :key="index" :name="proftag.name" />
     </div>
 </template>
 
 <style scoped>
-
 </style>
 
 <script>
