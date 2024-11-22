3d8897685aa134afd8081ce69f5c262d64b9725c
Nurik-XXX <nursultanarystan2004@gmail.com>
2024-11-22 11:54:23 +0500

    new_mentors

---
+++ src/components/Mentors/Mentors.vue
@@ -0,0 +1,17 @@
+<template>
+    <InfoHead />
+    <MentorsList/>
+</template>
+<script>
+import InfoHead from '../InfoHead.vue';
+import MentorsList from "@/components/Mentors/MentorsList.vue";
+export default {
+    components:{
+        InfoHead,
+        MentorsList
+    }
+}
+</script>
+<style scoped>
+
+</style>
